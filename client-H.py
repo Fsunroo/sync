cf584ecda3013a71388935dba061a9acf7d2003e
Mohammad Farhadi <Fsunroo@gmail.com>
2020-12-05 11:23:15 -0500

    Add files via upload

--- client-H.py
+++ client-H.py
@@ -29,15 +29,23 @@ def Send(file_name):
 print('[Connecting...]')
 s.connect((IP, PORT))                                   #1-Connect
 print(f'[Connected to {IP}:{PORT}]')
-
+print('[sending client File list]')
 s.send(" ".join(os.listdir(SHARED_FOLDER)).encode())    #2-Send Client Folder List
-
+print('[client File list send]')
+print('[reciving ServerOnly File list')
 get_list = s.recv(1024).decode().split()       #3-Recive ServerOnly Files List
+print(f'[ServerOnly File list recived: {get_list}')
+print('[reciving clientOnly File list')
 send_list = s.recv(1024).decode().split()     #4-Recive ClientOnly Files List 
+print(f'[ClientOnly File list recived: {send_list}')
 print(get_list)
 print(send_list)
 for file_name in get_list:
+    print(f'[Reciving {file_name} from server')
     Recive(file_name)
+    print(f'{file_name} seccssesfully! recived') 
 
 for file_name in send_list:
+    print(f'[sending {file_name}  to server')
     Send(file_name)
+    print(f'{file_name} seccssesfully! sent') 
